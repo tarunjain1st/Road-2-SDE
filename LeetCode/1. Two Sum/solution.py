2749f1d3e0ce197628fe4614a49f37f572087d82
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-04 11:47:19 +0530

    Create solution.py

---
+++ LeetCode/1. Two Sum/solution.py
@@ -0,0 +1,13 @@
+nums = [2,7,11,15]
+target = 9
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+            for i in range(len(nums)):
+                for j in range(i+1,len(nums)):
+                    if nums[i] + nums[j] ==  target:
+                        return[i,j]
+
+solve = Solution()
+result = solve.twoSum(nums, target)
+print(result)
