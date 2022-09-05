4b796da0d1d6014e28300740e432969d8afeea7e
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-05 15:19:20 +0530

    Update solution.py

--- LeetCode/383. Ransom Note/solution.py
+++ LeetCode/383. Ransom Note/solution.py
@@ -1,4 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if ransomNote == magazine:
+            return True
+        temp = list(ransomNote)
+        for i in magazine:
+            if i in temp and temp:
+                temp.remove(i)                
+        if len(temp) == 0:
+            return True
+        else:
+            return False
 
+ransomNote = "aa"
+magazine = "aab"
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.canConstruct(ransomNote, magazine)
 print(result)
