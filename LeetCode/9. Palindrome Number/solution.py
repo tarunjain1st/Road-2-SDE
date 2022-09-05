0b1390f062def410ec4d33814cb3c569416db0c7
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-05 15:32:57 +0530

    Update solution.py

--- LeetCode/9. Palindrome Number/solution.py
+++ LeetCode/9. Palindrome Number/solution.py
@@ -1,4 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num=x
+        temp=0
+        n=len(str(num))
+        for i in range(n):
+            digit = num%10
+            temp+=pow(10,n-i-1)*digit
+            num=num//10
+        return x==temp
 
+x = 121
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.isPalindrome(x)
 print(result)
