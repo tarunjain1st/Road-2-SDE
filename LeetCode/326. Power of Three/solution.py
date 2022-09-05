625303603375536c5e25f1321df453db456c05fe
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-05 15:23:34 +0530

    Update solution.py

--- LeetCode/326. Power of Three/solution.py
+++ LeetCode/326. Power of Three/solution.py
@@ -1,5 +1,16 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n > 0:
+            while(n!=1):
+                if n%3 != 0:
+                    return False
+                n = n/3
+            return True
 
+        else:
+            return False
 
+n = 27
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.isPowerOfThree(n)
 print(result)
