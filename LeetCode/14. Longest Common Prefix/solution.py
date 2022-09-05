e2d83f852c0f44e4b1c4a1d2df4f7b14fe7bc1ac
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-05 15:26:38 +0530

    Update solution.py

--- LeetCode/14. Longest Common Prefix/solution.py
+++ LeetCode/14. Longest Common Prefix/solution.py
@@ -1,4 +1,25 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        minSize = len(strs[0])
+        index = 0
+        result = ''
+        for i in range(len(strs)):
+            if minSize >= len(strs[i]):
+                minSize = len(strs[i])
+                index = i
+        if minSize == 0:
+            return result
+        if len(strs) == 1:
+            return strs[0]
+        for i in range(minSize):
+            temp = strs[index][i]
+            for j in range(len(strs)):
+                if strs[j][i] != temp:
+                    return result
+            result += temp
+        return result
 
+strs = ["flower","flow","flight"]
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.longestCommonPrefix(strs)
 print(result)
