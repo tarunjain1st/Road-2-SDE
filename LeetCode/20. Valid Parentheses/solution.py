1939bd64fb8eac0170967c3d70b368364a07cc7d
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-06 13:30:45 +0530

    Update solution.py

--- LeetCode/20. Valid Parentheses/solution.py
+++ LeetCode/20. Valid Parentheses/solution.py
@@ -1,5 +1,15 @@
-
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = [None]
+        for i in s:
+            if i in ['(','{','['] :
+                stack.append(i)
+            elif i==')' and stack[-1]=='(' or i==']' and stack[-1]=='[' or i=='}' and stack[-1]=='{' :
+                stack.pop() 
+            else: stack.append(i)
+        return stack.pop() is None
+        
+strs = "{}{}"
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.isValid(strs)
 print(result)
