f62b58b27d9039ec0e967f6496ea7a354d1b2f90
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-06 16:23:30 +0530

    Update solution.py

--- LeetCode/217. Contains Duplicate/solution.py
+++ LeetCode/217. Contains Duplicate/solution.py
@@ -1,4 +1,37 @@
+'''
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hashmap = {}
+        isTrue = False
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i]=1
+            else:
+                hashmap[i] += 1
+        print(hashmap)
+        for i in hashmap:
+            if hashmap[i]>1:
+                isTrue = True
+        return isTrue
+'''
+'''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+'''
 
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hashmap = {}
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i] = 1
+            else:
+                hashmap[i] += 1
+        return any(x > 1 for x in hashmap.values())
+
+
+nums = [1,2,3,4]
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.containsDuplicate(nums)
 print(result)
