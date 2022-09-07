e9098b67b492f6ef3bec84c728da730a4e172ddb
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-07 21:38:05 +0530

    Update solution.py

--- LeetCode/Q1. Two Sum/solution.py
+++ LeetCode/Q1. Two Sum/solution.py
@@ -1,12 +1,22 @@
 nums = [2,7,11,15]
 target = 9
-
+'''
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
             for i in range(len(nums)):
                 for j in range(i+1,len(nums)):
                     if nums[i] + nums[j] ==  target:
                         return[i,j]
+
+'''
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i, value in enumerate(nums):
+            if target-value in hashmap:
+                return [hashmap[target-value],i]
+            else: hashmap[value]=i
+
 
 solve = Solution()
 result = solve.twoSum(nums, target)
