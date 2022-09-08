50b2c11e598ac5d25f6453c45b3027bc1bd49472
Tarun Kumar Jain <tarunjain1st@gmail.com>
2022-09-08 22:05:31 +0530

    Update solution.py

--- LeetCode/Q94. Binary Tree Inorder Traversal/solution.py
+++ LeetCode/Q94. Binary Tree Inorder Traversal/solution.py
@@ -1,5 +1,31 @@
+#Definition for a binary tree node.
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def __init__(self):
+        self.data =[]
+    def inorderTraversal(self, root) -> list[int]:
+        if root:
+            temp=root
+            if temp.left:
+                self.inorderTraversal(temp.left)
+            self.data.append(temp.val)
+            if temp.right:
+                self.inorderTraversal(temp.right)
+
+        return self.data
+            
+        
+root = TreeNode(val=1)
+root.left = None
+root.right = TreeNode(val=2)
+root.right.left = TreeNode(val=3)
 
 solve = Solution()
-result = solve.twoSum(nums, target)
+result = solve.inorderTraversal(root)
 print(result)
