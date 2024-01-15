685f71ae355e3bef7de126171f6c3b34330e03e0
Tarun Kumar Jain <tarunjain1st@gmail.com>
2024-01-15 15:26:37 +0530

    Update Q2225. Find Players With Zero or One Losses.py

--- LeetCode/Q2225. Find Players With Zero or One Losses.py
+++ LeetCode/Q2225. Find Players With Zero or One Losses.py
@@ -13,10 +13,14 @@ for getting players who only wins we can remove loser from all players list, sam
 
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        # Count the occurrences of each losser & winner
         losser_freq = Counter(x[1] for x in matches)
         winner_freq = Counter(x[0] for x in matches)
 
+        # Find winners by subtracting the set of losers from the set of all winners
+        # Find losers by selecting players with a frequency of 1
         winners = set(winner_freq) - set(losser_freq)
-        lossers = {player for player, freq in losser_freq.items() if freq==1}
+        lossers = {player for player, freq in losser_freq.items() if freq == 1}
 
+        # Return sorted lists of winners and losers
         return sorted(winners), sorted(lossers)
